c6c41aba7c1402dee20915833779380c8548178f
Slava Dudkov <ya.mr.duma@gmail.com>
2017-01-27 18:18:55 +0800

    up

--- js/menu.js
+++ js/menu.js
@@ -11,17 +11,6 @@ MyApp.controller('PositionDemoCtrl', function DemoCtrl($mdDialog) {
 		originatorEv = ev;
 		$mdMenu.open(ev);
     };
-    this.announceClick = function(itemName) {
-		if (itemName == 'Настройки групп') {
-				window.location = 'settings.html';	
-		}else if (itemName == 'Расписание') {
-				window.location = 'tt.html';	
-		}  else if (itemName == 'Обратная связь') {
-				window.location = 'feedback.html';	
-		}else if (itemName == 'О приложении') {
-				window.location = 'about.html';	
-		}
-    };
 	this.announceLink = function(page) {
 		window.location = page + '.html';
     };
